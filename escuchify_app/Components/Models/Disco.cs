f331f5e3e51cf380b6eee3dcb698f2dbf28f4abf
Martin Rey <m.rey00@gmail.com>
2025-11-26 21:01:31 -0300

    Se vuelven a activar las validaciones en el modelo de Discos. Se sube el archivo de datos actualizado con los útlimos cambios.

--- escuchify_app/Components/Models/Disco.cs
+++ escuchify_app/Components/Models/Disco.cs
@@ -1,19 +1,18 @@
-using System.ComponentModel.DataAnnotations;
-
+// using System.ComponentModel.DataAnnotations;
 namespace escuchify_app.Components.Models;
 
 public class Disco
 {
     public int Id { get; set; }
     
-    // [Required(ErrorMessage = "El título es obligatorio")]
+    [Required(ErrorMessage = "El título es obligatorio")]
     public string Titulo { get; set; } = string.Empty;
     
-    // [Required(ErrorMessage = "El año de lanzamiento es obligatorio")]
-    // [Range(1900, 2025, ErrorMessage = "El año de lanzamiento debe estar entre 1900 y 2025")]
+    [Required(ErrorMessage = "El año de lanzamiento es obligatorio")]
+    [Range(1900, 2025, ErrorMessage = "El año de lanzamiento debe estar entre 1900 y 2025")]
     public int AnioLanzamiento { get; set; }
     
-    // [Required(ErrorMessage = "El tipo de disco es obligatorio")]
+    [Required(ErrorMessage = "El tipo de disco es obligatorio")]
     public string TipoDisco { get; set; } = string.Empty;
 }
 
